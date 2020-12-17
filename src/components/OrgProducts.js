45e88c501b8a1ac5d9c098f68729617f94f95115
Larkin <larkindsmith@gmail.com>
2020-12-17 16:59:34 -0500

    skeleton set up for products sections

---
+++ src/components/OrgProducts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SingleProduct from './SingleProduct';
+
+export default class OrgProducts extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Org Component</h3>
+        <SingleProduct />
+      </div>
+    );
+  }
+}
---
+++ src/components/ProductsSection.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import OrgProducts from './OrgProducts';
+
+export default class ProductsSection extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Products Section</h3>
+        <OrgProducts />
+      </div>
+    );
+  }
+}
